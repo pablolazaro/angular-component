a553e193b063acd4909476cf6f13c4d68edefef0
Pablo Lázaro <pablolp90@gmail.com>
2014-04-27 18:02:16 +0200

    Actualización de los test.

--- test/spec/services/resolver.spec.js
+++ test/spec/services/resolver.spec.js
@@ -9,6 +9,20 @@ describe('Unit: Testing ResolverService', function () {
         expect(ResolverService).not.toBe(undefined);
     }]));
 
+    it('ResolverService should contains convertToType method', inject(['ResolverService', function (ResolverService) {
+        expect(ResolverService.convertToType).not.toBe(undefined);
+    }]));
+
+    it('method convertToType should convert to String correctly', inject(['ResolverService', function (ResolverService) {
+        expect(ResolverService.convertToType(null, 'string')).toBe('null');
+        expect(ResolverService.convertToType(undefined, 'string')).toBe('undefined');
+        expect(ResolverService.convertToType({object: 'myObject'}, 'string')).toBe('{"object":"myObject"}');
+        expect(ResolverService.convertToType(['one', 'two', 'three'], 'string')).toBe('["one","two","three"]');
+        expect(ResolverService.convertToType(true, 'string')).toBe('true');
+        expect(ResolverService.convertToType(1, 'string')).toBe('1');
+        expect(ResolverService.convertToType('myString', 'string')).toBe('myString');
+    }]));
+
     it('method convertToType should convert to Boolean correctly', inject(['ResolverService', function (ResolverService) {
         expect(ResolverService.convertToType(true, 'boolean')).toBe(true);
         expect(ResolverService.convertToType(false, 'boolean')).toBe(false);
@@ -17,6 +31,41 @@ describe('Unit: Testing ResolverService', function () {
         expect(ResolverService.convertToType(1, 'boolean')).toBe(true);
         expect(ResolverService.convertToType(0, 'boolean')).toBe(false);
         expect(ResolverService.convertToType('stringNotBoolean', 'boolean')).toBe(false);
+        expect(ResolverService.convertToType({}, 'boolean')).toBe(false);
+        expect(ResolverService.convertToType([], 'boolean')).toBe(false);
+        expect(ResolverService.convertToType(null, 'boolean')).toBe(false);
+        expect(ResolverService.convertToType(undefined, 'boolean')).toBe(false);
     }]));
+
+    it('method convertToType should convert to Number correctly', inject(['ResolverService', function (ResolverService) {
+        expect(ResolverService.convertToType('123', 'number')).toBe(123);
+        expect(ResolverService.convertToType('-123', 'number')).toBe(-123);
+        expect(ResolverService.convertToType(true, 'number')).toBe(1);
+        expect(ResolverService.convertToType(false, 'number')).toBe(0);
+        expect(Number.isNaN(ResolverService.convertToType({}, 'number'))).toBe(true);
+        expect(Number.isNaN(ResolverService.convertToType([], 'number'))).toBe(true);
+        expect(Number.isNaN(ResolverService.convertToType(null, 'number'))).toBe(true);
+        expect(Number.isNaN(ResolverService.convertToType(undefined, 'number'))).toBe(true);
+    }]));
+
+    it('ResolverService should contains resolveCondition method', inject(['ResolverService', function (ResolverService) {
+        expect(ResolverService.resolveCondition).not.toBe(undefined);
+    }]));
+
+    it('ResolverService should resolve conditions correctly', inject(['ResolverService', function (ResolverService) {
+        var scope = {};
+
+        scope.myTrueVar = true;
+        scope.myFalseVar = false;
+        scope.myNumberVar = 1;
+        scope.myStringVar = 'true';
+
+        expect(ResolverService.resolveCondition('{{1 === 1}}', scope)).toBe(true);
+        expect(ResolverService.resolveCondition('{{0 === 1}}', scope)).toBe(false);
+        expect(ResolverService.resolveCondition('{{myTrueVar === true}}', scope)).toBe(true);
+        expect(ResolverService.resolveCondition('{{myFalseVar === true}}', scope)).toBe(false);
+        expect(ResolverService.resolveCondition('{{"true" === true}}', scope)).toBe(false);
+    }]));
+
 });
 
