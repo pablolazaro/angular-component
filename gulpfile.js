1519eee4526bc0c94c5f1f2523a1026c76601488
Pablo Lázaro <pablolp90@gmail.com>
2014-05-04 22:05:24 +0200

    Update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 'use strict';
 
-var gulp = require('gulp'),
+var gulp   = require('gulp'),
     concat = require('gulp-concat'),
-    clean = require('gulp-clean'),
-    ngmin = require('gulp-ngmin'),
+    clean  = require('gulp-clean'),
+    ngmin  = require('gulp-ngmin'),
     uglify = require('gulp-uglify'),
     header = require('gulp-header'),
-    karma = require('gulp-karma'),
-    bower = require('gulp-bower'),
-    src, dist;
+    karma  = require('gulp-karma'),
+    bower  = require('gulp-bower'),
+    debug  = require('gulp-debug'),
+    rename = require('gulp-rename'),
+    config = require('./config'),
+    pkg    = require('./package.json');
 
-var pkg = require('./package.json');
+
 var banner = [  '/**',
                 ' * <%= pkg.name %> - <%= pkg.description %>',
                 ' * @version v<%= pkg.version %>',
@@ -19,24 +22,29 @@ var banner = [  '/**',
                 ' */',
                 ''].join('\n');
 
+/**
+ * Task to clean distribution directory.
+ */
 gulp.task('clean', function () {
-    gulp.src('./dist/*.js', {read: false}).
-        pipe(clean());
+    return gulp.src(config.distributionFolder + '*.js', {read: false})
+        .pipe(clean());
 });
 
+/**
+ * Task to build component library.
+ */
 gulp.task('build', function () {
-    gulp.src('./module/**/*.js')
-        .pipe(concat('angular-component.js'))
-        .pipe(header(banner, { pkg : pkg } ))
-        .pipe(gulp.dest('./dist'));
+    gulp.src(config.sourceFolder + '**/*.js')
+        .pipe(concat(config.libraryName))            // Concat all JavaScript files into one file
+        .pipe(header(banner, { pkg : pkg } ))        // Add header comment to concated file
+        .pipe(gulp.dest(config.distributionFolder))  // Copy file to distribution folder
+        .pipe(ngmin())                               // Ngmin file to avoid conflict using UglifyJS
+        .pipe(uglify())                              // Compress file using UglifyJS
+        .pipe(header(banner, { pkg : pkg } ))        // Add header comment to minified file
+        .pipe(rename(config.minifiedLibraryName))    // Rename minified file
+        .pipe(gulp.dest(config.distributionFolder)); // Copy file to distribution folder
 });
 
-gulp.task('min', function () {
-    gulp.src('./dist/angular-component.js')
-        .pipe(ngmin())
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/angular-component.min.js'));
-});
 
 gulp.task('bower', function () {
     bower();
